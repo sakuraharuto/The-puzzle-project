ca15fba469f041ef7775c9634422c9b7787e7ec5
Ruocheng "Sam" Yaoxi <88380883+sakuraharuto@users.noreply.github.com>
2022-12-05 21:55:54 -0500

    Secret door hint on the screen

--- Assets/Scripts/SecretDoorController.cs
+++ Assets/Scripts/SecretDoorController.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SecretDoorController : MonoBehaviour
 {
     [SerializeField] PlayerStatus status;
     [SerializeField] Animator secretDoor;
     [SerializeField] bool isSecretDoorOpen;
+    [SerializeField] TextMeshProUGUI keyHint;
 
-    private void OnTriggerStay(Collider other) {
+
+    private void OnTriggerStay(Collider other)
+    {
         bool isPlayerKnowTheKey = status.GetTheKeyStatus();
+
+        if (other.tag == "Player" && isPlayerKnowTheKey)
+        {
+            keyHint.enabled = true;
+            keyHint.gameObject.SetActive(true);
+        }
 
         if (other.tag == "Player" && isPlayerKnowTheKey && !isSecretDoorOpen && Input.GetKeyDown(KeyCode.K))
         {
@@ -18,9 +28,18 @@ public class SecretDoorController : MonoBehaviour
         }
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        keyHint.enabled = false;
+        keyHint.gameObject.SetActive(false);
+    }
+
     void OpenTheSecretDoor()
     {
         secretDoor.Play("CupboardOpen");
         isSecretDoorOpen = true;
+        keyHint.enabled = false;
+        keyHint.gameObject.SetActive(false);
+        gameObject.SetActive(false);
     }
 }
